4bb14cab66aecd23b51b6a8ea8b61c36b72b3f6d
lightrain <lightrain@noreply.codeberg.org>
2023-01-21 00:45:25 +0000

    Release 1.0 (#5)
    Co-authored-by: lightrain <lightrain@humanity>
    Reviewed-on: https://codeberg.org/lightrain/renash/pulls/5

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_NAME="renash.py"
+BIN_DIR="$HOME/.local/bin"
+FILE_PATH="$BIN_DIR/$SCRIPT_NAME"
+
+# Make sure the script file exists
+if [ ! -f "$FILE_PATH" ]; then
+  echo "Error: $FILE_PATH not found"
+  exit 1
+fi
+
+# Remove the symbolic link in the bin directory
+rm "$FILE_PATH"
+
+echo "Uninstalled $SCRIPT_NAME from $BIN_DIR"
